c31820604eeb726629dfbffc55d4801852ec7410
Tony Cook <tony@develop-help.com>
2010-10-18 10:46:57 +0000

    Import of TONYC/Imager-Screenshot-0.010 from CPAN.
    gitpan-cpan-distribution: Imager-Screenshot
    gitpan-cpan-version:      0.010
    gitpan-cpan-path:         TONYC/Imager-Screenshot-0.010.tar.gz
    gitpan-cpan-author:       TONYC
    gitpan-cpan-maturity:     released

--- Screenshot.pm
+++ Screenshot.pm
@@ -10,7 +10,7 @@ push @ISA, 'Exporter';
 BEGIN {
   require Exporter;
   @ISA = qw(Exporter);
-  $VERSION = '0.009_001';
+  $VERSION = '0.010';
   eval {
     # try XSLoader first, DynaLoader has annoying baggage
     require XSLoader;
@@ -35,6 +35,7 @@ sub screenshot {
      top => 0,
      right => 0,
      bottom => 0,
+     monitor => 0,
      @_);
 
   my $result;
@@ -42,9 +43,10 @@ sub screenshot {
     defined &_win32
       or die "Win32 driver not enabled\n";
     $result = _win32($opts{hwnd}, $opts{decor}, $opts{left}, $opts{top},
-		     $opts{right}, $opts{bottom});
+		     $opts{right}, $opts{bottom}, $opts{monitor});
   }
   elsif (defined $opts{id}) { # X11 window id
+    exists $opts{display} or $opts{display} = 0;
     defined &_x11
       or die "X11 driver not enabled\n";
     $result = _x11($opts{display}, $opts{id}, $opts{left}, $opts{top},
@@ -87,7 +89,7 @@ sub screenshot {
   else {
     $result =
       defined &_win32 ? _win32(0, $opts{decor}, $opts{left}, $opts{top},
-			       $opts{right}, $opts{bottom}) :
+			       $opts{right}, $opts{bottom}, $opts{monitor}) :
       defined &_darwin ? _darwin($opts{left}, $opts{top},
 				 $opts{right}, $opts{bottom}) :
       defined &_x11 ? _x11($opts{display}, 0, $opts{left}, $opts{top},
@@ -179,11 +181,35 @@ Currently the image is always returned as a 24-bit image.
 
 =item screenshot hwnd => I<window handle>, decor => <capture decorations>
 
+=item screenshot hwnd => "active"
+
 Retrieve a screenshot under Win32, if I<window handle> is zero,
 capture the desktop.
 
 By default, window decorations are not captured, if the C<decor>
 parameter is set to true then window decorations are included.
+
+As of 0.010 hwnd can also be C<"active"> to capture the active (or
+"foreground") window.
+
+=item screenshot hwnd => 0
+
+Retrieve a screeshot of the default desktop under Win32.
+
+=item screenshot hwnd => 0, monitor => -1
+
+Retrieve a screenshot of all attached monitors under Win32.
+
+Note: this returns an image with an alpha channel, since there can be
+regions in the bounding rectangle of all monitors that no particular
+monitor covers.
+
+=item screenshot hwnd => 0, monitor => I<index>
+
+Retrieve a screenshot from a particular monitor under Win32.  A
+I<monitor> of zero is always treated as the primary monitor.
+
+If the given monitor is not active screenshot() will fail.
 
 =item screenshot id => I<window id>
 
